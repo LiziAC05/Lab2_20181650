abb5a4450999c05012e70507fa1488919b15a48a
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-15 19:12:21 -0500

    Vista Registrarse

---
+++ app/src/main/java/com/example/lab02_iot_20181650/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab02_iot_20181650;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
