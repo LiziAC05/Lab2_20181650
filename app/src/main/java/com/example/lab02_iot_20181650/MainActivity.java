356b5a7c2a5822134ef688819f114716dee72d0d
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-15 21:06:27 -0500

    El boton de inicio dirige para registrarse

--- app/src/main/java/com/example/lab02_iot_20181650/MainActivity.java
+++ app/src/main/java/com/example/lab02_iot_20181650/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lab02_iot_20181650;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 }
